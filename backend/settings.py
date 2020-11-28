fcdd06476c9a193a03d1f14b186e7a90f065620e
Aaron Raff <aaronraffdev@gmail.com>
2020-11-28 16:55:16 -0500

    move some settings to env vars

--- backend/settings.py
+++ backend/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "tqedc5f=6c1z5tkuubr_w9f9+9u+r99^uo*bz9&hxri15s1sv&"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "test_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", True)
 
 ALLOWED_HOSTS = []
 
